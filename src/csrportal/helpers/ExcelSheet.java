add6b76c308374aa8a630de877bdc4c998c67790
Wynton Franklin <wynton.franklin@gmail.com>
2018-08-01 19:39:40 -0400

    add wrapper for excelsheet creation

--- src/csrportal/helpers/ExcelSheet.java
+++ src/csrportal/helpers/ExcelSheet.java
@@ -5,7 +5,11 @@
  */
 package csrportal.helpers;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -16,32 +20,81 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
  */
 public class ExcelSheet{
     
+    private HSSFWorkbook workbook;
+    private HSSFSheet sheet;
+    private String sheetName="FirstSheet";
+    private String fileName="NewExcelFile.xls";
+    private int rowCounter =0;
+    private int totalColumns=0;
+    
+    public ExcelSheet(){
+        this.workbook = new HSSFWorkbook();
+        this.sheet = this.workbook.createSheet(sheetName);
+    }
+    
+    public void setHeader(String[] rows){
+         HSSFRow rowhead = getSheet().createRow((short)getCurrentRow());
+         this.setTotalColumns(rows.length);
+         for(int i=0; i<= rows.length-1; i++){
+            rowhead.createCell(i).setCellValue(rows[i]); 
+         }
+        this.updateRowCounter();
+    }
+    
+    public void addRow(String[] values){
+        HSSFRow row = getSheet().createRow((short)getCurrentRow());
+        for(int i=0; i<= this.getTotalColumns()-1; i++){
+            row.createCell(i).setCellValue(values[i]);
+        }
+        this.updateRowCounter();
+    }
+    
+    public void save() throws FileNotFoundException, IOException{
+        FileOutputStream fileOut = new FileOutputStream(fileName);
+        this.getWorkBook().write(fileOut);
+        fileOut.close();
+        this.getWorkBook().close();
+        System.out.println("Your excel file has been generated!");
+  
+    }
+    
+    public void setTotalColumns(int number){
+        totalColumns = number;
+    }
+    
+    public int getTotalColumns(){
+        return totalColumns;
+    }
+    
+    public HSSFSheet getSheet(){
+        return sheet;
+    }
+    
+    public int getCurrentRow(){
+        return rowCounter;
+    }
+    
+    public void updateRowCounter(){
+       rowCounter = rowCounter+1;
+    }
+    
+    public HSSFWorkbook getWorkBook(){
+        return this.workbook;
+    }
+    
+    
     public static void main(String[]args) {
+        ExcelSheet exsheet = new ExcelSheet();
+        String [] headers = {"Name","Job","Contact"};
+        String [] row = {"wynton","programmer","454-3353"};
+        String [] row2 = {"james","maanger","453-3533"};
+        exsheet.setHeader(headers);
+        exsheet.addRow(row);
+        exsheet.addRow(row2);
         try {
-            String filename = "NewExcelFile.xls" ;
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("FirstSheet");  
-
-            HSSFRow rowhead = sheet.createRow((short)0);
-            rowhead.createCell(0).setCellValue("No.");
-            rowhead.createCell(1).setCellValue("Name");
-            rowhead.createCell(2).setCellValue("Address");
-            rowhead.createCell(3).setCellValue("Email");
-
-            HSSFRow row = sheet.createRow((short)1);
-            row.createCell(0).setCellValue("1");
-            row.createCell(1).setCellValue("Sankumarsingh");
-            row.createCell(2).setCellValue("India");
-            row.createCell(3).setCellValue("sankumarsingh@gmail.com");
-
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            workbook.write(fileOut);
-            fileOut.close();
-            workbook.close();
-            System.out.println("Your excel file has been generated!");
-
-        } catch ( Exception ex ) {
-            System.out.println(ex);
+            exsheet.save();
+        } catch (IOException ex) {
+            Logger.getLogger(ExcelSheet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
